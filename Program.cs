e49057fa1700861e9b0f1d575269f45a644e4d91
Amanda Estrela <efernandes.amanda@gmail.com>
2022-05-28 16:07:55 -0300

    Estudos - reference type

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 ﻿using static System.Console;
 public class Program
 {
+    static void Demo1()
+    {  
+        int a = 2;       
+        a = Adicionar20(a);
+        WriteLine($"O valor da variável a é {a}");
+
+    }
     static int Adicionar20(int x)
     {
         return x + 20;
     }
+
+    static void TrocarNome(Pessoa p1, string novoNome)
+    {
+        p1.Nome = novoNome;
+    }
     public static void Main()
     {
-        int a = 2;
-        a = Adicionar20(a);
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Amanda";
+        p1.Idade = 30;
+        p1.Documento = "1234";
 
-        WriteLine($"O valor da variável a é {a}");
+        TrocarNome(p1, "Ana");
+
+        WriteLine($"O novo nome é: {p1.Nome}");
     }
+
+    
 }
